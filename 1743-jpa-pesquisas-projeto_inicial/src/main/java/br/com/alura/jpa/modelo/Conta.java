4f25a40073f3a872d4b4243f6e09eeaa99fb8bb2
Cassio Murilo <cmurilo.san@gmail.com>
2021-01-18 09:45:12 -0300

    Aula 01 - Relacionamento nas duas pontas

--- 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/modelo/Conta.java
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/modelo/Conta.java
@@ -1,9 +1,8 @@
 package br.com.alura.jpa.modelo;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 public class Conta {
@@ -14,13 +13,12 @@ public class Conta {
 	private Integer agencia;
 	private Integer numero;
 	private Double saldo;
+	@OneToMany
+	private List<Movimentacao> movimentacoes;
 
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
 	}
-	
-	
-
 
 	public Long getId() {
 		return id;
@@ -52,5 +50,9 @@ public class Conta {
 
 	public void setNumero(Integer numero) {
 		this.numero = numero;
+	}
+
+	public List<Movimentacao> getMovimentacoes() {
+		return movimentacoes;
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Estudo em JPA - JPQL e Criteria
+
+## Aula 01 - Relacionamentos bidirecionais
  
