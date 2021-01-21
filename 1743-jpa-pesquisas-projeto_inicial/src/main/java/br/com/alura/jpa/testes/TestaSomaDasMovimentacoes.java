a626458ecba75e1ef5e079458233850f4d34d1d4
Cassio Murilo <cmurilo.san@gmail.com>
2021-01-21 09:10:34 -0300

    Aula 03 - Sum, AVG e GroupBy

---
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
@@ -0,0 +1,23 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import java.math.BigDecimal;
+
+public class TestaSomaDasMovimentacoes {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
+        EntityManager em = emf.createEntityManager();
+
+        String jpql = "select avg(m.valor) from Movimentacao m";
+
+        TypedQuery<Double> query = em.createQuery(jpql, Double.class);
+        Double somaDasMovimentacoes = query.getSingleResult();
+
+        System.out.println("A soma das movimentações é: " + somaDasMovimentacoes);
+    }
+}
