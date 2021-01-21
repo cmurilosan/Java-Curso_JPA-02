387c25c4ee27ebacb01462329c163ec05f45eaf5
Cassio Murilo <cmurilo.san@gmail.com>
2021-01-21 10:31:49 -0300

    Aula 03 - Tipo específico

--- 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/testes/TesteMediaDiariaDasMovimentacoes.java
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/testes/TesteMediaDiariaDasMovimentacoes.java
@@ -1,9 +1,6 @@
 package br.com.alura.jpa.testes;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.TypedQuery;
+import javax.persistence.*;
 import java.util.List;
 
 public class TesteMediaDiariaDasMovimentacoes {
@@ -13,14 +10,17 @@ public class TesteMediaDiariaDasMovimentacoes {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("alura");
         EntityManager em = emf.createEntityManager();
 
-        String jpql = "select avg(m.valor) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
+        String jpql = "select avg(m.valor), day(m.data), month(m.data) from Movimentacao m group by day(m.data), month(m.data), year(m.data)";
 
-        TypedQuery<Double> query = em.createQuery(jpql, Double.class);
-        List<Double> mediaDasMovimentacoes = query.getResultList();
+        Query query = em.createQuery(jpql);
+        List<Object[]> mediaDasMovimentacoes = query.getResultList();
 
-        for (Double media :
+        for (Object[] resultado :
                 mediaDasMovimentacoes) {
-            System.out.println("A média das movimentações é: " + media);
+            System.out.println("A média das movimentações do dia " +
+                    resultado[1] + "/" +
+                    resultado[2] + " é: " +
+                    resultado[0]);
         }
     }
 }
