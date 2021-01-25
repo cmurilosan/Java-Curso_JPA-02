6d416fcdfcc31d1aa3d7e00a7fa9c704076bd9e3
Cassio Murilo <cmurilo.san@gmail.com>
2021-01-25 15:46:41 -0300

    Aula 05 - Criteria Query

--- 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/testes/TestaSomaDasMovimentacoes.java
@@ -2,8 +2,15 @@ package br.com.alura.jpa.testes;
 
 import br.com.alura.jpa.dao.ConnectionPersistence;
 import br.com.alura.jpa.dao.MovimentacaoDao;
+import br.com.alura.jpa.modelo.Movimentacao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
 
 public class TestaSomaDasMovimentacoes {
 
@@ -11,9 +18,17 @@ public class TestaSomaDasMovimentacoes {
 
         EntityManager em = new ConnectionPersistence().getEntityManager();
 
-        MovimentacaoDao dao = new MovimentacaoDao(em);
+        CriteriaBuilder builder = em.getCriteriaBuilder();
+        CriteriaQuery<BigDecimal> query = builder.createQuery(BigDecimal.class);
 
-        System.out.println("A soma das movimentações é: " + dao.getSomaDasMovimentacoes());
+        Root<Movimentacao> root = query.from(Movimentacao.class);
+
+        Expression<BigDecimal> sum = builder.sum(root.get("valor"));
+        query.select(sum);
+
+        TypedQuery<BigDecimal> typedQuery = em.createQuery(query);
+
+        System.out.println("A soma das movimentações é: " + typedQuery.getSingleResult());
     }
 
 }
