3a6286d8f2e608b406fa788c4df2a8322f804fb6
Cassio Murilo <cmurilo.san@gmail.com>
2021-01-25 11:57:49 -0300

    Aula 04 - @NamedQuery

--- 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/modelo/Movimentacao.java
@@ -5,13 +5,11 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
+
+@NamedQuery(name = "mediaDiariaMovimentacoes", query = "select new br.com.alura.jpa.modelo.MediaComData " +
+		"(avg(m.valor), day(m.data), month(m.data)) " +
+		"from Movimentacao m group by day(m.data), month(m.data), year(m.data)")
 
 @Entity
 public class Movimentacao {
