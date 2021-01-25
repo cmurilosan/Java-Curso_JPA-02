a8a1578f79dcf1701de01b71d4ec746e3c00b5d7
Cassio Murilo <cmurilo.san@gmail.com>
2021-01-25 15:29:01 -0300

    Aula 05 - O problema do JPQL

--- 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/dao/MovimentacaoDao.java
+++ 1743-jpa-pesquisas-projeto_inicial/src/main/java/br/com/alura/jpa/dao/MovimentacaoDao.java
@@ -1,6 +1,7 @@
 package br.com.alura.jpa.dao;
 
 import br.com.alura.jpa.modelo.MediaComData;
+import br.com.alura.jpa.modelo.Movimentacao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -12,6 +13,22 @@ public class MovimentacaoDao {
 
     public MovimentacaoDao(EntityManager em) {
         this.em = em;
+    }
+
+    public List<Movimentacao> getMovimentacoesFiltradasPorData (Integer dia, Integer mes, Integer ano) {
+        String jpql = "select m from Movimentacao m ";
+
+        if (dia != null) {
+            jpql = jpql + " day(m.data) = :pDia";
+        }
+        if (mes != null) {
+            jpql = jpql + " and month(m.data) = :pMes";
+        }
+        if (ano != null) {
+            jpql = jpql + " and year(m.data) = :pAno";
+        }
+
+        return null;
     }
 
     public List<MediaComData> getDiariaDasMovimentacoes() {
